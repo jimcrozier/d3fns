11e8afbaad008dca4f0ca923ae4657b948a86856
jimcrozier <crozierrj@gmail.com>
2014-01-29 17:58:27 -0500

    Update bubble1.js

--- bubble1/bubble1.js
+++ bubble1/bubble1.js
@@ -1,6 +1,24 @@
 <script src="http://d3js.org/d3.v3.min.js"></script>
 
-
+   function loadjscssfile(filename, filetype) {
+                if (filetype == "js") { //if filename is a external JavaScript file
+                   // alert('called');
+                    var fileref = document.createElement('script')
+                    fileref.setAttribute("type", "text/javascript")
+                    fileref.setAttribute("src", filename)
+                    alert('called');
+                }
+                else if (filetype == "css") { //if filename is an external CSS file
+                    var fileref = document.createElement("link")
+                    fileref.setAttribute("rel", "stylesheet")
+                    fileref.setAttribute("type", "text/css")
+                    fileref.setAttribute("href", filename)
+                }
+                if (typeof fileref != "undefined")
+                    document.getElementsByTagName("head")[0].appendChild(fileref)
+            }
+            
+      loadjscssfile("./bubble1.css") 
 
 var diameter = 960,
     format = d3.format(",d"),
