b9f4e55dd9c70b76191aba265f35244780b4c3b4
jimcrozier <crozierrj@gmail.com>
2014-02-03 11:40:44 -0500

    Update Barchart.js

--- Barchart/Barchart.js
+++ Barchart/Barchart.js
@@ -21,7 +21,7 @@ function closeIt(divIn) {
                }
                
          loadjscssfile("/js/D3/d3fns/Barchart/Barchart.css", "css") ;
-         loadjscssfile("/js/D3/d3fns/Barchart/tip.js","js")
+         loadjscssfile("/js/D3/d3fns/Barchart/tip.js","js");
          var margin = {top: 40, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
